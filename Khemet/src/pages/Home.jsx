0edb9e474159bc81e6ccdbe217fe33a9902d5f6e
NaglaaMohamed <naglaamohamed1486@gmail.com>
2026-07-10 21:09:35 +0300

    css mo3ak

--- Khemet/src/pages/Home.jsx
+++ Khemet/src/pages/Home.jsx
@@ -474,84 +474,38 @@ return (
           </g></svg>
             </Link>
           )}
-          {user?.role == "user" ?
-            (
-              <Link to="/trip-plan" className="tripsec-btn home-btn">Generate my trip
-                <svg
-            className="gen-svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            id="_24x24_On_Light_Next"
-            data-name="24x24/On Light/Next"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="#000000"
-            stroke="#000000"
-            strokeWidth="0.00024"
-          >
-          <g strokeWidth="0"></g>
-
-          <g strokeLinecap="round" strokeLinejoin="round"></g>
-
-          <g>
-            <rect
-              id="view-box"
+         {user?.role === "user" && (
+          <Link to="/trip-plan" className="tripsec-btn home-btn">
+            Generate my trip
+            <svg
+              className="gen-svg"
               width="24"
               height="24"
-              fill="#3A2A1A"
-              opacity="0"
-            />
+              viewBox="0 0 24 24"
+              fill="#000000"
+            >
+              <path
+                d="M10.22,9.28a.75.75,0,0,1,0-1.06l2.72-2.72H.75A.75.75,0,0,1,.75,4H12.938L10.22,1.281A.75.75,0,1,1,11.281.22l4,4a.749.749,0,0,1,0,1.06l-4,4a.75.75,0,0,1-1.061,0Z"
+                transform="translate(4.25 7.25)"
+                fill="#3A2A1A"
+              />
+            </svg>
+          </Link>
+        )}
 
-            <path
-              id="Shape"
-              d="M10.22,9.28a.75.75,0,0,1,0-1.06l2.72-2.72H.75A.75.75,0,0,1,.75,4H12.938L10.22,1.281A.75.75,0,1,1,11.281.22l4,4a.749.749,0,0,1,0,1.06l-4,4a.75.75,0,0,1-1.061,0Z"
-              transform="translate(4.25 7.25)"
-              fill="#3A2A1A"
-            />
-          </g></svg></Link>
-            ) : (<>
-            <Link to="/" className="tripsec-btn home-btn" onClick={(e) => {
-    if (user.role == "admin") {
-      e.preventDefault();
-      showToast("error", "This section isn't part of the admin responsibilities.");
-    }
-  }}>
-               Generate my trip
-                <svg
-            className="gen-svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            id="_24x24_On_Light_Next"
-            data-name="24x24/On Light/Next"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="#000000"
-            stroke="#000000"
-            strokeWidth="0.00024"
+        {user?.role === "admin" && (
+          <Link
+            to="/"
+            className="tripsec-btn home-btn"
+            onClick={(e) => {
+              e.preventDefault();
+              showToast("error", "This section isn't part of the admin responsibilities.");
+            }}
           >
-          <g strokeWidth="0"></g>
-
-          <g strokeLinecap="round" strokeLinejoin="round"></g>
-
-          <g>
-            <rect
-              id="view-box"
-              width="24"
-              height="24"
-              fill="#3A2A1A"
-              opacity="0"
-            />
-
-            <path
-              id="Shape"
-              d="M10.22,9.28a.75.75,0,0,1,0-1.06l2.72-2.72H.75A.75.75,0,0,1,.75,4H12.938L10.22,1.281A.75.75,0,1,1,11.281.22l4,4a.749.749,0,0,1,0,1.06l-4,4a.75.75,0,0,1-1.061,0Z"
-              transform="translate(4.25 7.25)"
-              fill="#3A2A1A"
-            />
-          </g></svg>
-              </Link> 
-            </>)}
-         
+            Generate my trip
+          </Link>
+        )}
+                
           </div>
           </div>
       </section>
